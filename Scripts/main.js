f9d1d5f925835d717b7387f724aa3c6d12ed9e2a
Safwan-Hayaat <safwanhayaat@gmail.com>
2026-02-26 11:41:45 +0600

    Uploaded the website
    obviously not finished, i just uploaded this now so everyone working on it can see it

---
+++ Scripts/main.js
@@ -0,0 +1,30 @@
+function updateHeaderPosition() {
+  const header = document.getElementById('header');
+  
+  /* this is a javascipt code to make sure that the website look fine on mobile, basically the headers position is fixed.
+   But if the headers height is too big then a large portion of the screen is basically invisible.
+   so this code makes sure that when the header height is over 30%, the position becomes static. */
+  if (!header) return;
+
+  const viewportHeight = window.innerHeight;
+  const headerHeight = header.offsetHeight;
+
+
+  if (headerHeight > viewportHeight * 0.4) {
+    header.classList.remove('fixed-header');
+    header.classList.add('normal-header');
+  } else {
+    header.classList.remove('normal-header');
+    header.classList.add('fixed-header');
+  }
+}
+
+
+window.addEventListener('DOMContentLoaded', updateHeaderPosition);
+window.addEventListener('resize', updateHeaderPosition);
+window.addEventListener()
+
+updateHeaderPosition();
+
+
+
